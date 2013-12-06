92bd533f57595f75efc9dfd1c5474ed2dc6624f7
Piotr Podusowski <podusowski@gmail.com>
2013-12-06 21:14:43 +0100

    run_tests fix

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,14 @@
 for i in tests/*; do
-    pushd $i
-    if ./test.sh ; then
-        echo pass
-    else
-        echo fail
-        exit 1
+    if [ -d $i ]; then
+        pushd $i
+        if ./test.sh ; then
+            echo pass
+        else
+            echo fail
+            exit 1
+        fi
+        popd
+        echo
+        echo
     fi
-    popd
-    echo
-    echo
 done
