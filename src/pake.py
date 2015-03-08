48204097d3693e90ca2775428035708b35ed6490
Piotr Podusowski <podusowski@gmail.com>
2015-03-08 20:54:43 +0100

    removed duplicated

--- src/pake.py
+++ src/pake.py
@@ -18,24 +18,6 @@ import compiler
 import targets
 
 """
-    utilities
-"""
-
-def execute(command, capture_output = False):
-    out = ''
-    try:
-        if capture_output:
-            out = subprocess.check_output(command, shell=True)
-        else:
-            subprocess.check_call(command, shell=True)
-    except subprocess.CalledProcessError:
-        raise Exception("command did not finish successfully: " + command)
-        #ui.fatal("command did not finish successfully: " + command)
-
-    ui.debug("command completed: " + command)
-    return out
-
-"""
     targets
 """
 class CommonTargetParameters:
