1a4680fd38cc05ff028b41901a2a61be8a0f969c
Piotr Podusowski <podusowski@gmail.com>
2015-03-08 21:51:14 +0100

    cleanup

--- src/pake.py
+++ src/pake.py
@@ -45,158 +45,8 @@ class CxxParameters:
         self.compiler_flags = []
         self.built_targets = []
 
-"""
-    parser
-"""
-
-class VariableDeposit:
-    def __init__(self):
-        self.modules = {}
-
-    def export_special_variables(self, configuration):
-        ui.debug("exporting special variables")
-        ui.push()
-
-        self.add_empty("__configuration", "$__null")
-        self.add("__configuration", "$__name", parsing.Token.make_literal(configuration.name))
-        for (value, name) in configuration.export:
-            self.add("__configuration", name.content, value)
-
-        for module in self.modules:
-            self.add(module, "$__build", parsing.Token(parsing.Token.LITERAL, fsutils.build_dir(configuration.name)))
-
-        ui.pop()
-
-    def pollute_environment(self, current_module):
-        ui.debug("polluting environment")
-        ui.push()
-        for module in self.modules:
-            for (name, variable) in self.modules[module].iteritems():
-                evaluated = self.eval(module, variable)
-                env_name = module + "_" + name[1:]
-                os.environ[env_name] = " ".join(evaluated)
-                ui.debug("  " + env_name + ": " + str(evaluated))
-                if module == current_module:
-                    env_short_name = name[1:]
-                    os.environ[env_short_name] = " ".join(evaluated)
-                    ui.debug("  " + env_short_name + ": " + str(evaluated))
-        ui.pop()
-
-    def eval(self, current_module, l):
-        ui.debug("evaluating " + str(l) + " in context of module " + current_module)
-        ui.push()
-
-        ret = []
-        for token in l:
-            if token.is_a(parsing.Token.LITERAL):
-                content = self.__eval_literal(current_module, token.content)
-                ui.debug("  " + token.content + " = " + content)
-                ret.append(content)
-            elif token.is_a(parsing.Token.VARIABLE):
-                parts = token.content.split(".")
-
-                ui.debug("dereferencing " + str(parts))
-
-                module = ''
-                name = ''
-                if len(parts) == 1:
-                    module = current_module
-                    name = parts[0]
-                elif len(parts) == 2:
-                    module = parts[0][1:] # lose the $
-                    name = "$" + parts[1]
-
-                if not module in self.modules:
-                    ui.parse_error(msg="no such module: " + module)
-
-                # TODO: make some comment about __configuration variables
-                if not name in self.modules[module]:
-                    ui.fatal("dereferenced " + name + " but it doesn't exists in module " + module)
-
-                for value in self.modules[module][name]:
-                    if value.is_a(parsing.Token.VARIABLE):
-                        re = self.eval(module, [value])
-                        for v in re: ret.append(v)
-                    else:
-                        content = self.__eval_literal(module, value.content)
-                        ret.append(content)
-                        ui.debug("    = " + str(content))
-            else:
-                ui.parse_error(token)
-
-        ui.debug(" = " + str(ret))
-        ui.pop()
-        return ret
-
-    def __eval_literal(self, current_module, s):
-        ui.debug("evaluating literal: " + s)
-        ui.push()
-        ret = ""
-
-        STATE_READING = 1
-        STATE_WAITING_FOR_PARENTHESIS = 2
-        STATE_READING_NAME = 3
-
-        variable_name = '$'
-        state = STATE_READING
-
-        for c in s:
-            if state == STATE_READING:
-                if c == "$":
-                    state = STATE_WAITING_FOR_PARENTHESIS
-                else:
-                    ret += c
-            elif state == STATE_WAITING_FOR_PARENTHESIS:
-                if c == "{":
-                    state = STATE_READING_NAME
-                else:
-                    ui.parse_error(msg="expecting { after $")
-            elif state == STATE_READING_NAME:
-                if c == "}":
-                    ui.debug("variable: " + variable_name)
-                    evaluated_variable = self.eval(current_module, [parsing.Token(parsing.Token.VARIABLE, variable_name)])
-                    ret += " ".join(evaluated_variable)
-                    variable_name = '$'
-                    state = STATE_READING
-                else:
-                    variable_name += c
-            elif state == STATE_READING_NAME:
-                variable_name = variable_name + c
-
-        ui.pop()
-        return ret
-
-    def add_empty(self, module_name, name):
-        ui.debug("adding empty variable in module " + module_name + " called " + name)
-
-        if not module_name in self.modules:
-            self.modules[module_name] = {}
-
-        self.modules[module_name][name] = []
-
-
-    def add(self, module_name, name, value):
-        ui.debug("adding variable in module " + module_name + " called " + name + " with value of " + str(value))
-
-        if not module_name in self.modules:
-            self.modules[module_name] = {}
-
-        self.modules[module_name][name] = [value]
-
-    def append(self, module_name, name, value):
-        ui.debug("appending variable in module " + module_name + " called " + name + " with value of " + str(value))
-
-        if not module_name in self.modules:
-            self.modules[module_name] = {}
-
-        if not name in self.modules[module_name]:
-            self.modules[module_name][name] = []
-
-        self.modules[module_name][name].append(value)
-        ui.debug("  new value: " + str(self.modules[module_name][name]))
-
 class Module:
-    def __init__(self, jobs, _variable_deposit, configuration_deposit, target_deposit, filename):
+    def __init__(self, jobs, configuration_deposit, target_deposit, filename):
         assert isinstance(filename, str)
 
         ui.debug("parsing " + filename)
@@ -518,7 +368,7 @@ modules = [] #TODO: do we need to store it?
 
 def parse_source_tree(jobs, configuration_deposit, target_deposit):
     for filename in find_pake_files():
-        module = Module(jobs, None, configuration_deposit, target_deposit, filename)
+        module = Module(jobs, configuration_deposit, target_deposit, filename)
         modules.append(module)
 
     configuration = configuration_deposit.get_selected_configuration()
