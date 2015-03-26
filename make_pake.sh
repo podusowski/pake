53aca3d4f365733410644e134baaca50800c74f5
Jakub Draganek <salwator@gmail.com>
2015-03-26 16:21:56 +0100

    using python2 when executing waffled pake

--- make_pake.sh
+++ make_pake.sh
@@ -2,4 +2,7 @@ root=`pwd`
 mkdir -p __build
 pushd scripts
 ./waffle_maker.py -o $root/__build/pake.py -p $root/src/ -s $root/src/pake.py
+(echo "#!/usr/bin/env python2"; tail -n +2 "$root/__build/pake.py") > new_pake
+mv new_pake $root/__build/pake.py
+chmod +x $root/__build/pake.py
 popd
