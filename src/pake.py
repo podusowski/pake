bbb877d40811c173baa362d410a000e9afeef574
Piotr Podusowski <podusowski@gmail.com>
2015-03-30 20:52:05 +0200

    function extracted

--- src/pake.py
+++ src/pake.py
@@ -6,6 +6,7 @@ import targets
 import variables
 import configurations
 import parser
+import command_line
 
 def parse_source_tree():
     for filename in fsutils.pake_files:
@@ -14,21 +15,25 @@ def parse_source_tree():
     configuration = configurations.get_selected_configuration()
     variables.export_special_variables(configuration)
 
-def main():
-    import command_line
 
+def _build_some_targets_if_requested():
+    if command_line.args.target:
+        for target in command_line.args.target:
+            targets.build(target)
+        return True
+    elif command_line.args.all:
+        targets.build_all()
+        return True
+
+
+def main():
     parse_source_tree()
 
     configuration = configurations.get_selected_configuration()
     if configuration.name != "__default":
         ui.bigstep("configuration", str(configurations.get_selected_configuration()))
 
-    if command_line.args.target:
-        for target in command_line.args.target:
-            targets.build(target)
-    elif command_line.args.all:
-        targets.build_all()
-    else:
+    if not _build_some_targets_if_requested():
         ui.info("no target selected\n")
 
         ui.info(ui.BOLD + "targets:" + ui.RESET)
