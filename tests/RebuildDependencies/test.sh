46b0a279e70dc9905c2850afa33af739e1bdb6ca
Piotr Podusowski <podusowski@gmail.com>
2015-04-05 12:20:00 +0200

    test refactor

--- tests/RebuildDependencies/test.sh
+++ tests/RebuildDependencies/test.sh
@@ -2,57 +2,53 @@
 
 rm -rf __build
 
-rm __build/calls.list
 assert $pake hello
-assert grep -e -c.*main.cpp __build/calls.list
-assert grep -e -c.*foo __build/calls.list
-assert grep -e -o.*hello __build/calls.list
+assert grep -e -c.*main.cpp __build/calls.list > /dev/null
+assert grep -e -c.*foo __build/calls.list > /dev/null
+assert grep -e -o.*hello __build/calls.list > /dev/null
 assert __build/__default/hello
 
 rm __build/calls.list
 assert $pake hello
-assert_fail grep -e -c.*main.cpp __build/calls.list
-assert_fail grep -e -c.*foo __build/calls.list
-assert_fail grep -e -o.*hello __build/calls.list
+assert_fail test -f __build/calls.list
 
-rm __build/calls.list
 assert touch main.cpp
 assert $pake hello
-assert grep -e -c.*main.cpp __build/calls.list
-assert_fail grep -e -c.*foo __build/calls.list
-assert grep -e -o.*hello __build/calls.list
+assert grep -e -c.*main.cpp __build/calls.list > /dev/null
+assert_fail grep -e -c.*foo __build/calls.list > /dev/null
+assert grep -e -o.*hello __build/calls.list > /dev/null
 assert __build/__default/hello
 
 rm __build/calls.list
 assert touch utils.hpp
 assert $pake hello
-assert grep -e -c.*main.cpp __build/calls.list
-assert_fail grep -e -c.*foo __build/calls.list
-assert grep -e -o.*hello __build/calls.list
+assert grep -e -c.*main.cpp __build/calls.list > /dev/null
+assert_fail grep -e -c.*foo __build/calls.list > /dev/null
+assert grep -e -o.*hello __build/calls.list > /dev/null
 assert __build/__default/hello
 
 rm __build/calls.list
 assert touch foo.cpp
 assert $pake hello
-assert_fail grep -e -c.*main.cpp __build/calls.list
-assert grep -e -c.*foo __build/calls.list
-assert grep -e -o.*hello __build/calls.list
+assert_fail grep -e -c.*main.cpp __build/calls.list > /dev/null
+assert grep -e -c.*foo __build/calls.list > /dev/null
+assert grep -e -o.*hello __build/calls.list > /dev/null
 assert __build/__default/hello
 
 rm __build/calls.list
 assert touch foo.hpp
 assert $pake hello
-assert grep -e -c.*main.cpp __build/calls.list
-assert grep -e -c.*foo __build/calls.list
-assert grep -e -o.*hello __build/calls.list
+assert grep -e -c.*main.cpp __build/calls.list > /dev/null
+assert grep -e -c.*foo __build/calls.list > /dev/null
+assert grep -e -o.*hello __build/calls.list > /dev/null
 assert __build/__default/hello
 
 rm __build/calls.list
 assert touch hello.pake
 assert $pake hello
-assert grep -e -c.*main.cpp __build/calls.list
-assert grep -e -c.*foo __build/calls.list
-assert grep -e -o.*hello __build/calls.list
+assert grep -e -c.*main.cpp __build/calls.list > /dev/null
+assert grep -e -c.*foo __build/calls.list > /dev/null
+assert grep -e -o.*hello __build/calls.list > /dev/null
 assert __build/__default/hello
 
 rm -rf __build
