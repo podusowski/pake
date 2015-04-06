ce1db171a81eab65b7a6d57a819304deb2bbfb7c
Piotr Podusowski <podusowski@gmail.com>
2015-04-06 16:15:05 +0200

    more reliable test for https://github.com/podusowski/pake/issues/20 #2

--- tests/rebuild_after_deep_header_gets_moved/test.sh
+++ tests/rebuild_after_deep_header_gets_moved/test.sh
@@ -5,18 +5,18 @@ mkdir -p __build
 
 pushd __build
 
-    echo "// nothing" > header.hpp
+    echo "// nothing" > deeper_header.hpp
+    echo -e "#include \"deeper_header.hpp\"" > header.hpp
     echo -e "#include \"header.hpp\"\nint main() {}" > test.cpp
     echo "target application test sources(test.cpp)" > test.pake
 
     assert $pake test
     assert __build/__default/test
 
-    echo "int main() {}" > test.cpp
-    rm header.hpp
+    echo -e "#include \"new_deeper_header.hpp\"" > header.hpp
+    mv deeper_header.hpp new_deeper_header.hpp
 
     assert $pake test
-    assert __build/__default/test
 
 popd
 
